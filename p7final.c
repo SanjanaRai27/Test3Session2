c2ebeaf601ba9a58f2f0145b274df7a60d1f2e41
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-03-03 08:50:45 +0000

    p7done

--- p7final.c
+++ p7final.c
@@ -8,10 +8,10 @@ int gcd(int a,int b)
 {
   int gcd(int a,int b);
   int t;
-    while (b!=0)
+    while (b !=0)
       {
         t = b;
-        b = a % b;
+        b = a%b;
         a = t;
         }
     return a;
