bbb26e567e75dec0d9daf31ec30a18084e212c5d
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-03-31 07:44:14 +0000

    p1 done

--- p1final.c
+++ p1final.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void input(int *num1,int *den1,int *num2,int *den2)
 {
-  printf("Enter the two fractions\n");
+  printf("enter the two fractions\n");
   scanf("%d%d %d%d",num1,den1,num2,den2);
 }
 void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
